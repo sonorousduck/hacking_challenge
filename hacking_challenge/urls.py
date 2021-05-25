60021aefeee786ec2a57c13eeb4c9eb30f168bbd
Ryan <sonorousduck@gmail.com>
2021-05-25 10:21:27 -0600

    Added a favicon.ico

--- hacking_challenge/urls.py
+++ hacking_challenge/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('challenges/', include('challenges.urls')),
     path('playground/', include('playground.urls')),
     path('', include('homepage.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'))),
 
 ]
