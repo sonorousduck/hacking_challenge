6ac74c54705a6e8aec08ad1c0d902feabd30b06f
Ryan <sonorousduck@gmail.com>
2021-06-23 16:02:30 -0600

    Worked on implementing achievements. I  think these will be really fun for the hacking challenge

--- achievements/models.py
+++ achievements/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Achievements(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    achievementIcon = models.ImageField()
+    
+
+
+    def __str__(self):
+        return f"{self.title}"
+
+
