beb8b8e7a61f80c504a6c414dfbe3ece1153cb22
Ryan <sonorousduck@gmail.com>
2021-07-01 12:05:46 -0600

    Fixed issue

--- loginSignup/views.py
+++ loginSignup/views.py
@@ -47,7 +47,6 @@ def signUp(request):
                             email=form.cleaned_data['email'],
                             first_name=form.cleaned_data['firstName'],
                             last_name = form.cleaned_data['lastName'],
-                            customText="Look at who is too cool for flavor text or something! Psh! (Ironically, this is your flavor text",
                             )
 
                     challengesJSON = []
@@ -86,7 +85,7 @@ def signUp(request):
                     JSONIncorrect = json.dumps(incorrectPerChallenge)
 
 
-                    customUser = CustomUser(numChallenges=Challenge.objects.all().count(), completedChallenges=0, challenges=JSONchallenges, incorrectPerChallenge=JSONIncorrect, user=userCreated, last_name=form.cleaned_data['lastName'], achievements=JSONAchievements)
+                    customUser = CustomUser(numChallenges=Challenge.objects.all().count(), completedChallenges=0, challenges=JSONchallenges, incorrectPerChallenge=JSONIncorrect, user=userCreated, last_name=form.cleaned_data['lastName'], achievements=JSONAchievements, customText="Look at who is too cool for flavor text or something! Psh! (Ironically, this is your flavor text")
 
 
                     userCreated.save()
