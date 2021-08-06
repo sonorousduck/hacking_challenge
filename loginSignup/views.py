710c21be208854be55940ba775cdf61153fc8de5
Ryan <sonorousduck@gmail.com>
2021-08-06 08:51:05 -0600

    Small bug squashed

--- loginSignup/views.py
+++ loginSignup/views.py
@@ -80,7 +80,7 @@ def signUp(request):
 
                     JSONchallenges = json.dumps(challengesJSON)
                     JSONIncorrect = json.dumps(incorrectPerChallenge)
-                    numberRequiredChallenges = Challenge.objects.all().filter(optional=False).count()
+                    numberRequiredChallenges = Challenge.objects.all().filter(optionalChallenge=False).count()
 
 
                     customUser = CustomUser(numChallenges=Challenge.objects.all().count(), numRequiredChallenges=numberRequiredChallenges, completedChallenges=0, challenges=JSONchallenges, incorrectPerChallenge=JSONIncorrect, user=userCreated, first_name=form.cleaned_data['firstName'], last_name=form.cleaned_data['lastName'], achievements=JSONAchievements, customText="Look at who is too cool for flavor text or something! Psh! (Ironically, this is your flavor text")
