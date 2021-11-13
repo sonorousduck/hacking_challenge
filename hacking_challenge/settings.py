957db679e13ba7c7af5d0194cdd6c3c947e2c0e9
Hacking Challenge <hackingchallengeserver@docker-s-1vcpu-1gb-sfo2-01>
2021-11-13 15:20:56 -0700

    Fixed some errors that I should have a while ago

--- hacking_challenge/settings.py
+++ hacking_challenge/settings.py
@@ -25,7 +25,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^4r)j(iwa$21&+pd3yc)e3d(np@(az6a2&(pwrr2j#^nyg9d=f'
+#SECRET_KEY = os.environ['SECRET_KEY']
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
